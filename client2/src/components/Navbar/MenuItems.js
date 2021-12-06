8cf51216dd93321028e11b50b54e4089fa8e08c0
Shopnil Shahriar <shopnilshahriar@Shopnils-MacBook-Pro.local>
2021-12-05 20:27:16 -0500

    changed nav bar items

--- client2/src/components/Navbar/MenuItems.js
+++ client2/src/components/Navbar/MenuItems.js
@@ -1,20 +1,5 @@
 export const MenuItems = [
     {
-        title: 'Main Menu',
-        url: '#',
-        cName: 'nav-links'
-    },
-    {
-        title: 'Login Out',
-        url: '#',
-        cName: 'nav-links'
-    },
-    {
-        title: 'Manage Account',
-        url: '#',
-        cName: 'nav-links'
-    },
-    {
         title: 'Other thing',
         url: '#',
         cName: 'nav-links-mobile'
